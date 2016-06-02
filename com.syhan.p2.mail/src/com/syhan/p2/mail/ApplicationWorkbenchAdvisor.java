cff2942a2a4f3cfec63181873642ab7aac1b3f8b
Daniel Han <syhan@nextree.co.kr>
2016-06-02 18:08:05 +0900

    initial commit

---
+++ com.syhan.p2.mail/src/com/syhan/p2/mail/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,27 @@
+package com.syhan.p2.mail;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipselabs.p2.rcpupdate.utils.P2Util;
+
+/**
+ * This workbench advisor creates the window advisor, and specifies
+ * the perspective id for the initial window.
+ */
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+	
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return Perspective.ID;
+	} 
+	
+	@Override
+	public void preStartup() {
+	    P2Util.checkForUpdates();
+	}
+	
+}
